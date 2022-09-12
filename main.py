811df9280175ec3fc55780c5056e4a709f16a258
Datarist <hello@datarist.com>
2022-09-12 21:01:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -171,9 +171,9 @@ def statistics_correlation_bland_altman_plot_and_analysis():
 def statistics_correlation_box_cox_transformation_for_simple_linear_regression():
     return render_template('statistics/correlation/box-cox-transformation-for-simple-linear-regression.html')
 
-@app.route('/statistics/correlation/xxxx/')
-def statistics_correlation_xxxx():
-    return render_template('statistics/correlation/xxxx.html')
+@app.route('/statistics/correlation/canonical-correlation/')
+def statistics_correlation_canonical_correlation():
+    return render_template('statistics/correlation/canonical-correlation.html')
 
 @app.route('/statistics/correlation/xxxx/')
 def statistics_correlation_xxxx():
