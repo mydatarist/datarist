82faf0af3becd955fbe22073076adfe57426b57a
Datarist <hello@datarist.com>
2022-09-14 15:21:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -429,9 +429,9 @@ def statistics_distribution_fitting_exponential_probability_plots():
 def statistics_distribution_fitting_gamma_distribution_fitting():
     return render_template('statistics/distribution-fitting/gamma-distribution-fitting.html')
 
-@app.route('/statistics/distribution-fitting/')
-def statistics_distribution_fitting_index():
-    return render_template('statistics/distribution-fitting/index.html')
+@app.route('/statistics/distribution-fitting/grubbs-outlier-test/')
+def statistics_distribution_fitting_grubbs_outlier_test():
+    return render_template('statistics/distribution-fitting/grubbs-outlier-test.html')
 
 @app.route('/statistics/distribution-fitting/')
 def statistics_distribution_fitting_index():
