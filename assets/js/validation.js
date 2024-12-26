202014f028faa1e6454f72d8d1dc688e611a1c8a
Datarist <admin@datarist.com>
2024-12-26 22:37:01 +0300

    Validation JS Update

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,13 +1,1 @@
-(() => {
-  'use strict'
-  const forms = document.querySelectorAll('.needs-validation')
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+(()=>{"use strict";const t=document.querySelectorAll(".needs-validation");Array.from(t).forEach((t=>{t.addEventListener("submit",(e=>{t.checkValidity()||(e.preventDefault(),e.stopPropagation()),t.classList.add("was-validated")}),!1)}))})();
