10d5580a77b96a07b8f86e57f499a136d33aab61
Datarist <data@datarist.com>
2025-01-22 11:26:57 +0300

    Shutdown

--- assets/js/validation.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";const t=document.querySelectorAll(".needs-validation");Array.from(t).forEach((t=>{t.addEventListener("submit",(e=>{t.checkValidity()||(e.preventDefault(),e.stopPropagation()),t.classList.add("was-validated")}),!1)}))})();
