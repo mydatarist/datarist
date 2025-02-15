a59f774300be4b0fc3551d331ea056266cc1e6de
Datarist <data@datarist.com>
2025-02-15 21:05:25 +0300

    Page.js

---
+++ app/page.js
@@ -0,0 +1,20 @@
+"use client";
+
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+export default function App() {  
+  return (
+    <main>
+      <h1>Data Insights</h1>
+    </main>
+  );
+}
