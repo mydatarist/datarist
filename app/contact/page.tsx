a1bcb573178f8c61137794a8949a64a769145020
Datarist <data@datarist.com>
2025-02-15 20:16:35 +0300

    Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -13,7 +13,7 @@ Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
-export default function App() {
+export default function Page() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
