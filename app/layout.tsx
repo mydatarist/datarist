db90db08624b6b2d802605832784bb0b61486350
Datarist <data@datarist.com>
2025-02-16 21:56:18 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,13 +13,9 @@ export default function RootLayout({
         {children} 
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div className="container">
-            <div className="col-md-4 d-flex align-items-center">
-              <Link className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" href="/">
-                <svg className="bi" width="30" height="24"></svg>
-              </Link>
-              <span className="mb-3 mb-md-0 text-body-secondary">&copy; 2025 Datarist, Inc.</span>
-            </div>
             <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+              <li className="ms-3"><Link className="text-body-secondary" href="/"><svg className="bi" width="30" height="24"></svg></Link></li>
+              <li className="ms-3 text-body-secondary">&copy; 2025 Datarist, Inc.</li>
               <li className="ms-3"><Link className="text-body-secondary" href="/terms/">Terms</Link></li>
               <li className="ms-3"><Link className="text-body-secondary" href="/privacy/">Privacy</Link></li>
               <li className="ms-3"><Link className="text-body-secondary" href="/cookies/">Cookies</Link></li>
