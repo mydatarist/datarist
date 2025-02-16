377f8cad31adff9848ab41aa402e4e9b9e7fe995
Datarist <data@datarist.com>
2025-02-16 22:22:28 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,14 +11,14 @@ export default function RootLayout({
       <body>
         <Link href="https://datarist.com">Datarist</Link>
         {children} 
-        <footer className="d-flex flex-wrap py-3 my-4 border-top">
+        <footer className="py-3 my-4 border-top">
           <div className="container">
-            <ul className="nav list-unstyled d-flex">
-              <li className="text-body-secondary"><Link className="text-body-secondary" href="/"><svg className="bi" width="30" height="24"></svg></Link>&copy; 2025 Datarist, Inc.</li>
-              <li className="ms-3"><Link className="text-body-secondary" href="/terms/">Terms</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary" href="/privacy/">Privacy</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary" href="/cookies/">Cookies</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary" href="/contact/">Contact</Link></li>               
+            <ul className="nav list-unstyled">
+              <li className="text-body-secondary"><Link className="text-body-secondary text-decoration-none" href="/"><svg className="bi" width="30" height="24"></svg></Link>&copy; 2025 Datarist, Inc.</li>
+              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/terms/">Terms</Link></li>
+              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/privacy/">Privacy</Link></li>
+              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/cookies/">Cookies</Link></li>
+              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/contact/">Contact</Link></li>               
             </ul>            
           </div>         
         </footer>                
