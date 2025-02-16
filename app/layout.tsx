b1c4e84c33d617749505f4df46f94716d10428c0
Datarist <data@datarist.com>
2025-02-16 22:15:16 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
         <footer className="d-flex flex-wrap py-3 my-4 border-top">
           <div className="container">
             <ul className="nav list-unstyled d-flex">
-              <li className="ms-3 text-body-secondary"><Link className="text-body-secondary" href="/"><svg className="bi" width="30" height="24"></svg></Link>&copy; 2025 Datarist, Inc.</li>
+              <li className="text-body-secondary"><Link className="text-body-secondary" href="/"><svg className="bi" width="30" height="24"></svg></Link>&copy; 2025 Datarist, Inc.</li>
               <li className="ms-3"><Link className="text-body-secondary" href="/terms/">Terms</Link></li>
               <li className="ms-3"><Link className="text-body-secondary" href="/privacy/">Privacy</Link></li>
               <li className="ms-3"><Link className="text-body-secondary" href="/cookies/">Cookies</Link></li>
