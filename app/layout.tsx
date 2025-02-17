0a9e276ee5c824cef90cc5680257c24d818def05
Datarist <data@datarist.com>
2025-02-17 16:33:27 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,20 +13,24 @@ export default function RootLayout({
         {children} 
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div className="container-fluid">
-          <ul className="nav col-md-6 justify-content-start list-unstyled d-flex">
-              <li className="text-body-secondary"><Link className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" href="/"><svg width="30" height="24"></svg></Link> &copy; 2025 Datarist, Inc.</li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/terms/">Terms</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/privacy/">Privacy</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/cookies/">Cookies</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/contact/">Contact</Link></li>               
-            </ul>
-            <ul className="nav col-md-6 justify-content-end list-unstyled d-flex">
-              <li className="text-body-secondary"><Link className="text-body-secondary text-decoration-none" href="/"><svg width="30" height="24"></svg></Link> &copy; 2025 Datarist, Inc.</li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/terms/">Terms</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/privacy/">Privacy</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/cookies/">Cookies</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/contact/">Contact</Link></li>               
-            </ul> 
+            <div className="col-md-6 justify-content-start list-unstyled d-flex">
+              <ul className="nav">
+                <li className="text-body-secondary"><Link className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" href="/"><svg width="30" height="24"></svg></Link> &copy; 2025 Datarist, Inc.</li>
+                <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/terms/">Terms</Link></li>
+                <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/privacy/">Privacy</Link></li>
+                <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/cookies/">Cookies</Link></li>
+                <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/contact/">Contact</Link></li>               
+              </ul>
+            </div>
+            <div className="col-md-6 justify-content-end list-unstyled d-flex">
+              <ul className="nav">
+                <li className="text-body-secondary"><Link className="text-body-secondary text-decoration-none" href="/"><svg width="30" height="24"></svg></Link> &copy; 2025 Datarist, Inc.</li>
+                <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/terms/">Terms</Link></li>
+                <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/privacy/">Privacy</Link></li>
+                <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/cookies/">Cookies</Link></li>
+                <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/contact/">Contact</Link></li>               
+              </ul>
+            </div>             
           </div>  
         </footer>                                
       </body>
