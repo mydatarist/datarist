735af57d85c0bd81ecf747f80409f52e97338de5
Datarist <data@datarist.com>
2025-02-17 15:43:34 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ export default function RootLayout({
       <body>
         <Link href="https://datarist.com">Datarist</Link>
         {children} 
-        <footer className="py-3 my-4 border-top">
-            <ul className="nav list-unstyled">
-              <li className="text-body-secondary"><Link className="text-body-secondary text-decoration-none" href="/"><svg width="30" height="24"></svg></Link>&copy; 2025 Datarist, Inc.</li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/terms/">Terms</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/privacy/">Privacy</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/cookies/">Cookies</Link></li>
-              <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/contact/">Contact</Link></li>               
-            </ul>        
-        </footer>                
+        <div className="container">
+          <ul className="nav list-unstyled">
+            <li className="text-body-secondary"><Link className="text-body-secondary text-decoration-none" href="/"><svg width="30" height="24"></svg></Link> &copy; 2025 Datarist, Inc.</li>
+            <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/terms/">Terms</Link></li>
+            <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/privacy/">Privacy</Link></li>
+            <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/cookies/">Cookies</Link></li>
+            <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/contact/">Contact</Link></li>               
+          </ul> 
+        </div>                          
       </body>
     </html>
   );
