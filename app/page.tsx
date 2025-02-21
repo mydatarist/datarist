025fb1521fd9e9828a4b8d18a5040f82e0cc4970
Datarist <data@datarist.com>
2025-02-21 22:59:30 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Datarist: Data Insights",
+}
+
 export default function App() {  
   return (
     <main>
