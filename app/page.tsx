7e52cdfeebc973d5d5a6b4616c84c84affb9a783
Datarist <data@datarist.com>
2025-02-21 22:10:51 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,5 @@
 "use client";
 
-import type { Metadata } from "./layout";
-
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-
-export const metadata: Metadata = {
-  title: "Datarist",
-}
-
-Amplify.configure(outputs);
-
-const client = generateClient<Schema>();
-
 export default function App() {  
   return (
     <main>
