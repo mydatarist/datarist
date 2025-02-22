1fa1e5e1b34ab076b0a76423ad9a2a47035cc2c6
Datarist <data@datarist.com>
2025-02-22 20:30:24 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Datarist: Data Insights",
   description: "Empowering people and organizations with data.",
+  canonical: "https://datarist.com",
 }
 
 export default function App() {  
