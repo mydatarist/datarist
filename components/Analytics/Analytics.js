d9410d13ae054907cc2d167d229db21636b83c62
Datarist <data@datarist.com>
2025-02-23 17:12:22 +0300

    Analytics

--- components/Analytics/Analytics.js
+++
@@ -1,9 +0,0 @@
-<Script src={'https://www.googletagmanager.com/gtag/js?id=G-RS1PX7109Y'} strategy="afterInteractive" />
-<Script id="google-analytics" strategy="afterInteractive">
-    {'
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-RS1PX7109Y');
-    '}          
-</Script>
