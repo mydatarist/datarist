22ecdab61736b40991a4ead03753ab77ad2f99d0
Datarist <data@datarist.com>
2025-02-23 12:10:53 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-RS1PX7109Y" />
-        <Script>
-          window.dataLayer = window.dataLayer || [];
+        <Script src={'https://www.googletagmanager.com/gtag/js?id=G-RS1PX7109Y'} strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {'
+            window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-RS1PX7109Y');
+          '}          
         </Script>
       </head>
       <body>
