e6dd1ac888db6e99b32d46238eb52de41776d872
Datarist <data@datarist.com>
2025-02-23 18:45:17 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,16 +16,16 @@ export default function RootLayout({
           {"window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}"}
         </Script>
         <Script src={`https://www.googletagmanager.com/gtag/js?id=G-RS1PX7109Y`} strategy="afterInteractive" />
-          <Script id='google-analytics' strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-RS1PX7109Y');
+        <Script id='google-analytics' strategy="afterInteractive" 
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || []; 
+              function gtag(){dataLayer.push(arguments);} 
+              gtag('js', new Date()); 
+              gtag('config', 'G-RS1PX7109Y');
                 `,
-                }}
-            />
+          }}
+        />
       </head>
       <body>
         <Link href="https://datarist.com">Datarist</Link>
