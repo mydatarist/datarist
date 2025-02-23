7cf846faec4f42b846709b7729fe1bf9e8e426b8
Datarist <data@datarist.com>
2025-02-23 13:24:36 +0300

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Script from "next/script";
+import Analytics from "@components/Analytics/Analytics";
 import "./globals.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -11,15 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script src={'https://www.googletagmanager.com/gtag/js?id=G-RS1PX7109Y'} strategy="afterInteractive" />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {'
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-RS1PX7109Y');
-          '}          
-        </Script>
+        <Analytics />
       </head>
       <body>
         <Link href="https://datarist.com">Datarist</Link>
---
+++ components/Analytics/Analytics.js
@@ -0,0 +1,4 @@
+<Script src={'https://www.googletagmanager.com/gtag/js?id=G-RS1PX7109Y'} strategy="afterInteractive" />
+<Script id="google-analytics" strategy="afterInteractive">
+            
+</Script>
