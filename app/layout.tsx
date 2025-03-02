90383025cd835a41332ba368cfaa90b48f92010a
Datarist <data@datarist.com>
2025-03-02 21:59:50 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
             <div className="d-flex">
               <div className="dd-search" id="docsearch" data-dd-docs-version="5.3">
               
+              </div>
             </div>
           </nav>
         </header>       
