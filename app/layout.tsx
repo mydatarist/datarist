11ba46b2ff7e38a7b92807e5fee2c70a4ce60a9f
Datarist <data@datarist.com>
2025-03-03 18:25:00 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
               <li><Link href="/cookies/" className="nav-link px-2 link-body-emphasis">Cookies</Link></li>
               <li><Link href="/contact/" className="nav-link px-2 link-body-emphasis">Contact</Link></li>
             </ul>
+            <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+
+            </form>
             <div className="text-end">
               <button type="button" className="btn btn-outline-light me-2">Login</button>
               <button type="button" className="btn btn-warning">Sign-up</button>
