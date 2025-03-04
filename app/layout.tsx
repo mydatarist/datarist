16236e79b5c9741b03555485d2b94afe4cc2402f
Datarist <data@datarist.com>
2025-03-04 21:52:30 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,8 @@ export default function RootLayout({
         <footer className="py-3 my-4 border-top footer text-body-secondary">
           <div className="container-fluid">
             <ul className="nav">
-              <li className="ms-1 text-body-secondary">&copy; 2025 Datarist, Inc.</li>
+              document.getElementById("year").innerHTML = new Date().getFullYear();
+              <li className="ms-1 text-body-secondary">&copy; <span id="year"></span> Datarist, Inc.</li>
               <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/terms/">Terms</Link></li>
               <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/privacy/">Privacy</Link></li>
               <li className="ms-3"><Link className="text-body-secondary text-decoration-none" href="/cookies/">Cookies</Link></li>
