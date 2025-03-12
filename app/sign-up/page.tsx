4b7ed61af690ec7bbe91a323440520807020aa53
Datarist <data@datarist.com>
2025-03-12 22:25:02 +0300

    Update page.tsx

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from "next";
+import type { FormEvent } from "react"
+import { Amplify } from "aws-amplify"
+import { signUp } from "aws-amplify/auth"
+import outputs from "../amplify_outputs.json"
 
 export const metadata: Metadata = {
   title: "Sign up",
@@ -11,10 +15,35 @@ export const metadata: Metadata = {
   },
 }
 
-export default function Page() {  
+Amplify.configure(outputs)
+
+interface SignUpFormElements extends HTMLFormControlsCollection {
+  email: HTMLInputElement
+  password: HTMLInputElement
+}
+
+interface SignUpForm extends HTMLFormElement {
+  readonly elements: SignUpFormElements
+}
+
+export default function App() {
+  async function handleSubmit(event: FormEvent<SignUpForm>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    // ... validate inputs
+    await signUp({
+      username: form.elements.email.value,
+      password: form.elements.password.value,
+    })
+  }
+
   return (
-    <main>
-      <h1>Create your free account.</h1>
-    </main>
-  );
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email:</label>
+      <input type="text" id="email" name="email" />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      <input type="submit" />
+    </form>
+  )
 }
