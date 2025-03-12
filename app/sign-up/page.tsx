3b53e0e79c8c6b7b1de6970b03af6810f2931529
Datarist <data@datarist.com>
2025-03-12 21:48:25 +0300

    Update page.tsx

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,39 +1,22 @@
 "use client";
 
-import type { FormEvent } from "react"
-import { Amplify } from "aws-amplify"
-import { signUp } from "aws-amplify/auth"
-import outputs from "../amplify_outputs.json"
+import type { Metadata } from "next";
 
-Amplify.configure(outputs)
-
-interface SignUpFormElements extends HTMLFormControlsCollection {
-  email: HTMLInputElement
-  password: HTMLInputElement
+export const metadata: Metadata = {
+  title: "Sign up",
+  description: "Create your free account",
+  alternates: {
+    canonical: "https://datarist.com/sign-up/",
+    languages: {
+      "en-us": "https://datarist.com/en-us/",
+    },
+  },
 }
 
-interface SignUpForm extends HTMLFormElement {
-  readonly elements: SignUpFormElements
-}
-
-export default function App() {
-  async function handleSubmit(event: FormEvent<SignUpForm>) {
-    event.preventDefault()
-    const form = event.currentTarget
-    // ... validate inputs
-    await signUp({
-      username: form.elements.email.value,
-      password: form.elements.password.value,
-    })
-  }
-
+export default function Page() {  
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email:</label>
-      <input type="text" id="email" name="email" />
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" name="password" />
-      <input type="submit" />
-    </form>
-  )
+    <main>
+      <h1>Create your free account.</h1>
+    </main>
+  );
 }
