c8600b6687465c9a2dee8b2d10d78918aad96005
Datarist <data@datarist.com>
2025-03-13 16:05:47 +0300

    Update page.tsx

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -27,6 +27,15 @@ interface SignUpForm extends HTMLFormElement {
 }
 
 export default function Page() {  
+  async function handleSubmit(event: FormEvent<SignUpForm>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    // ... validate inputs
+    await signUp({
+      username: form.elements.email.value,
+      password: form.elements.password.value,
+    })
+  }
   return (
     <main>
       <h1>Create your free account.</h1>
