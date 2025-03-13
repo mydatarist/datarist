bd55907319a5e5be489b449279b5b2bcf67efb69
Datarist <data@datarist.com>
2025-03-13 14:38:44 +0300

    Update page.tsx

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign up",
+  description: "Create your free account.",
+  alternates: {
+    canonical: "https://datarist.com/sign-up/",
+    languages: {
+      "en-us": "https://datarist.com/en-us/",
+    },
+  },
+}
+
+export default function Page() {  
+  return (
+    <main>
+      <h1>Create your free account.</h1>
+    </main>
+  );
+}
