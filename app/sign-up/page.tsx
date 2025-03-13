f723f0f2d6920a860152353f7a57e48983f0fbf1
Datarist <data@datarist.com>
2025-03-13 15:44:02 +0300

    Update page.tsx

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -15,10 +15,37 @@ export const metadata: Metadata = {
   },
 }
 
-export default function Page() {  
+Amplify.configure(outputs)
+
+interface SignUpFormElements extends HTMLFormControlsCollection {
+  email: HTMLInputElement
+  password: HTMLInputElement
+}
+
+interface SignUpForm extends HTMLFormElement {
+  readonly elements: SignUpFormElements
+}
+
+export default function App() {
+  async function handleSubmit(event: FormEvent<SignUpForm>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    // ... validate inputs
+    await signUp({
+      username: form.elements.email.value,
+      password: form.elements.password.value,
+    })
+  }  
   return (
     <main>
       <h1>Create your free account.</h1>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email:</label>
+      <input type="text" id="email" name="email" />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      <input type="submit" />
+    </form>
     </main>
   );
 }
