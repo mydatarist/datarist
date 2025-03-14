6846ba10c3c21770a7f139fba6fd8e659a70bc2c
Datarist <data@datarist.com>
2025-03-14 21:40:45 +0300

    Update page.tsx

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,51 +1,21 @@
-import type { FormEvent } from "react";
-import { Amplify } from "aws-amplify";
-import { signUp } from "aws-amplify/auth";
-import outputs from "@/amplify_outputs.json";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Sign up",
-  description: "Create your free account.",
-  alternates: {
-    canonical: "https://datarist.com/sign-up/",
-    languages: {
-      "en-us": "https://datarist.com/en-us/",
-    },
-  },
-}
-
-Amplify.configure(outputs)
-
-interface SignUpFormElements extends HTMLFormControlsCollection {
-  email: HTMLInputElement
-  password: HTMLInputElement
-}
-
-interface SignUpForm extends HTMLFormElement {
-  readonly elements: SignUpFormElements
-}
-
-export default function Page() {  
-  async function handleSubmit(event: FormEvent<SignUpForm>) {
-    event.preventDefault()
-    const form = event.currentTarget
-    // ... validate inputs
-    await signUp({
-      username: form.elements.email.value,
-      password: form.elements.password.value,
-    })
-  }
+import { signup } from '@/app/actions/auth'
+ 
+export function SignupForm() {
   return (
-    <main>
-      <h1>Create your free account.</h1>
-      <form>
-      <label htmlFor="email">Email:</label>
-      <input type="text" id="email" name="email" />
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" name="password" />
-      <input type="submit" />
-      </form>
-    </main>
-  );
+    <form action={signup}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input id="name" name="name" placeholder="Name" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" placeholder="Email" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  )
 }
