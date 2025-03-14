34d6b7011d607f312a1209b311614eaeb8d394c0
Datarist <data@datarist.com>
2025-03-14 21:20:34 +0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,61 +1,20 @@
-import sgMail from '@sendgrid/mail'
+import type { Metadata } from "next";
 
-// Set the SendGrid API key
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+export const metadata: Metadata = {
+  title: "Contact",
+  description: "Let's talk about your project.",
+  alternates: {
+    canonical: "https://datarist.com/contact/",
+    languages: {
+      "en-us": "https://datarist.com/en-us/",
+    },
+  },
+}
 
-export default function ContactForm() {
-    // State for the form fields
-
-    // State for the form status
-    const [status, setStatus] = useState('')
-
-    // Handle form submission
-    const handleSubmit = async (formData) => {
-        "user server"
-        const rawFormData = {
-            to: formData.get("email"),
-            from: "your_sending_email",
-            subject: `New message from ${formData.get("name")}`,
-            text: formData.get("message"),
-        }
-
-        //Here need to write email sending functionality
-        try {
-            await sgMail.send(rawFormData)
-            // Send a success response
-            console.log("Email Send Successfully!")
-        } catch (error) {
-            // Send an error response
-            console.log("Something went wrong, please try again!")
-        }
-    }
-
-    return (
-        <form action={handleSubmit}>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    id="name"
-                    value={name}
-                />
-            </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    value={email}
-                />
-            </div>
-            <div>
-                <label htmlFor="message">Message</label>
-                <textarea
-                    id="message"
-                    value={message}
-                />
-            </div>
-            <button type="submit">Send</button>
-        </form>
-    )
+export default function Page() {  
+  return (
+    <main>
+      <h1>Contact Datarist</h1>
+    </main>
+  );
 }
